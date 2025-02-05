3ebf92ad48c8efbca4cbc7664c12f29c11bec92f
Leonardo Pereira <leonardobrpereira2015@gmail.com>
2025-02-05 16:49:01 -0300

    initial commit

---
+++ source/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Project.Domain.Entities;
+
+namespace Project.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    public DbSet<User> User { get; set; }
+    public DbSet<Role> Role { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
